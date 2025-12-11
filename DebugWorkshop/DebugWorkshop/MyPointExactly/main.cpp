3c57e01f0a5f295906dd612eb5a72bf9b92adb05
tomer-pelech <tomer.pelech@gmail.com>
2025-12-11 18:25:25 +0200

    all memory check code didnt work

--- DebugWorkshop/DebugWorkshop/MyPointExactly/main.cpp
+++ DebugWorkshop/DebugWorkshop/MyPointExactly/main.cpp
@@ -1,16 +1,6 @@
 #include "Point.h"
 #include <iostream>
-#define _CRTDBG_MAP_ALLOC
-#include <cstdlib>
-#include <crtdbg.h>
 
-#ifdef _DEBUG
-#define DBG_NEW new ( _NORMAL_BLOCK , __FILE__ , __LINE__ )
-// Replace _NORMAL_BLOCK with _CLIENT_BLOCK if you want the
-// allocations to be of _CLIENT_BLOCK type
-#else
-#define DBG_NEW new
-#endif
 
 int main()
 {
@@ -22,7 +12,5 @@ int main()
 	p2 = p2;
 	std::cout << "p2=" << p2 << std::endl;
 
-	_CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_DEBUG);
-	_CrtDumpMemoryLeaks();
 	return 0;
 }
