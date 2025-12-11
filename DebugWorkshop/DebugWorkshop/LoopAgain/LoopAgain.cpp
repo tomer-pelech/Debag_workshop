8be862f03b53100fc8ca923a474ec51a6b4bedeb
tomer-pelech <tomer.pelech@gmail.com>
2025-12-11 18:40:36 +0200

    made it run phibonachi without doing infinate loop nor printing negative numbers

--- DebugWorkshop/DebugWorkshop/LoopAgain/LoopAgain.cpp
+++ DebugWorkshop/DebugWorkshop/LoopAgain/LoopAgain.cpp
@@ -4,16 +4,16 @@ int main()
 {
 	unsigned int size = 0;
 
-	int t1 = 0, t2 = 1;
+	unsigned int t1 = 0, t2 = 1;
 
 	std::cout << "what is the size of the series? ";
 	std::cin >> size;
 
-	while (size >= 0)
+	while (size > 0)
 	{
 		std::cout << t1 << ", ";
 
-		int nextTerm = t1 + t2;
+		unsigned int nextTerm = t1 + t2;
 		t1 = t2;
 		t2 = nextTerm;
 
