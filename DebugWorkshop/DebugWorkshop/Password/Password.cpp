3d489902ae723015e03d6bcf2d776bfc51bf8cc7
tomer-pelech <tomer.pelech@gmail.com>
2025-12-11 19:45:22 +0200

    wrote password

--- DebugWorkshop/DebugWorkshop/Password/Password.cpp
+++ DebugWorkshop/DebugWorkshop/Password/Password.cpp
@@ -14,7 +14,9 @@ int main()
 	std::cout << "Enter your password to continue:" << std::endl;
 	Password pwd;
 	std::cin >> pwd.value;
-
+	//if you input 16 charachters it will print congrats
+	//it happens because the std::cin adds a null char at the end of the str
+	//which in turn causes an overflow in this situation and makes boolian false
 	if (!strcmp(pwd.value, "********"))
 		pwd.incorrect = false;
 
